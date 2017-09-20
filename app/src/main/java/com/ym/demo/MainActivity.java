bbf9de9f177c964919a553a412cafdf6147c4060
xm2016 <1060578743@qq.com>
2017-09-20 13:06:00 +0800

    Initial commit

---
+++ app/src/main/java/com/ym/demo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ym.demo;
+
+import android.app.Instrumentation;
+import android.os.Bundle;
+import android.support.test.uiautomator.UiDevice;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Thread thread = new Thread(this);
+        thread.start();
+
+
+    }
+    @Override
+    public void run() {
+        Instrumentation instrumentation = new Instrumentation();
+        UiDevice device = UiDevice.getInstance(instrumentation);
+        device.pressHome();
+    }
+}
